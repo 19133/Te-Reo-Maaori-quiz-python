892a61ca24b8579bc89af2e79f17015d07bb287a
Kafi Ashkir <82185473+19133@users.noreply.github.com>
2021-08-17 02:32:21 +0000

    put questions into a list and added used a while loop so the questions repeat if the user choses incorrectly.

---
+++ quiz_component3.py
@@ -0,0 +1,29 @@
+import random
+
+
+# quiz questions 
+Questions_answers = {
+    "What is ocean in Te Reo Maaori? \n A. kātao\n B. Moana\n C. Awa \n D. Wai":"b",
+    "What is red in Te Reo Maaori?  \n A. whero \n B. Red \n C. whrea \n D. rato" :"a",
+    "If it is Ua it is what? \n A. Rainy \n B. Sunny \n C. hot \n D. 21 hours ":"a"
+}   
+
+score = 0
+question = list (Questions_answers.keys())
+#input answer here 
+
+while True:
+  answer = input('Answer ' )
+  # if correct, moves onto next question
+  if answer.lower() == Questions_answers:
+      print("Correct Answer")
+      print ()
+      # if correct, score goes up by 1
+      score + 1
+      print("total score is", score)
+      print()
+      break
+  else:
+      #if wrong, Asks the same question again
+      print("Wrong Answer, try again")
+question.remove
