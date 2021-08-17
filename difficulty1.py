84cc10c813154b491f498fbeb4af43f5f48c5609
Kafi Ashkir <82185473+19133@users.noreply.github.com>
2021-08-17 02:51:06 +0000

    added difficulty component so user can choose how hard/easy the quiz can be

---
+++ difficulty1.py
@@ -0,0 +1,14 @@
+user_difficulty = input("What difficulty would you like to play. Easy, normal, or hard\n").lower()
+
+if user_difficulty == "easy":
+  print ("Correct")
+
+elif user_difficulty == "medium":
+  print ("Correct")
+
+elif user_difficulty == "hard":
+  print ("Correct")
+
+else:
+  print("Incorrect")
+
--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-from import *
+from difficulty1 import *
