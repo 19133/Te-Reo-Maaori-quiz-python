370709bd479e3a3caf5f4c51de91b802a40eaf81
Kafi Ashkir <82185473+19133@users.noreply.github.com>
2021-08-17 02:24:28 +0000

    turned quiz component into a function (define statement)

---
+++ quiz_component1.py
@@ -0,0 +1,13 @@
+quiz_question = input("What is red in Te Reo Maaori\n").lower()
+if quiz_question == "whero":
+  print ("Correct")
+else:
+  print("Incorrect")
+
+quiz_question2 = input ("What’s ocean in Te Reo Maaori?").lower()
+
+if quiz_question2 == "Moana":
+  print ("Correct")
+
+else:
+  print ("Incorrect") 
---
+++ quiz_component2.py
@@ -0,0 +1,50 @@
+# red question function
+def quiz_question(question):
+  valid=False
+  while not valid:
+    user_response = input (question) .lower()
+
+    if user_response == "whero":
+        user_response = "whero"
+        return user_response 
+
+    else:
+      print("Incorrect, try again") 
+
+# ocean question function
+def quiz_question2(question):
+  valid=False
+  while not valid:
+    user_response = input (question) .lower()
+
+    if user_response == "moana":
+        user_response = "moana"
+        return user_response 
+
+    else:
+      print("Incorrect, try again") 
+
+
+# main routine goes here
+
+# whats red in te reo maaori? question componenet
+qna_whero = quiz_question ("What’s red in Te Reo Maaori?\n")
+
+if qna_whero == "whero":
+  print("Correct")
+  print()
+
+else:
+  print("Incorect, try again")
+  print()
+
+# whats ocean in te reo maaori? question componenet
+qna_awa = quiz_question2 ("What’s ocean in Te Reo Maaori?\n")
+
+if qna_awa == "moana":
+  print("Correct")
+  print()
+
+else:
+  print("Incorect, try again")
+  print()
