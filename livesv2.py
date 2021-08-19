838e81d312f0de2bd844eed5cd58189b83888466
Kafi Ashkir <82185473+19133@users.noreply.github.com>
2021-08-19 09:11:36 +0000

    added lives -= 1 when the user gets a question wrong but doesn't work that well

---
+++ livesv2.py
@@ -0,0 +1,48 @@
+import random
+
+play_lives = input ("Would you like to play with lives\n")
+
+# if yes, the user will be asked how many lives would they like to have
+if play_lives == "yes":
+  print ("Thats great")
+  print("How many lives would you like to play with")
+  user_lives = int(input()) 
+  print("You will be playing with {} lives".format(user_lives))
+
+
+else:
+  print("You will be playing with 0 lives")
+  user_lives = ()
+
+# quiz questions 
+Questions_answers = {
+    "What is ocean in Te Reo Maaori? \n A. kātao\n B. Moana\n C. Awa \n D. Wai":["b", "moana"],
+    "What is red in Te Reo Maaori?  \n A. whero \n B. Red \n C. whrea \n D. rato" :["a", "whero"],
+    "If it is Ua it is what? \n A. Rainy \n B. Sunny \n C. hot \n D. 21 hours ":["a", "rainy"]
+}   
+
+score = 0
+question = list (Questions_answers.keys())
+#input answer here 
+while True:
+    if not question:
+        break
+    ques = random.choice(question)
+    print(ques)
+    while True:
+        answer = input('Answer ' )
+        # if correct, moves onto next question
+        if answer.lower() in Questions_answers[ques]:
+            print("Correct Answer")
+            print ()
+            # if correct, score goes up by 1
+            score + 1
+            print("total score is", score)
+            print()
+            break
+        else:
+            #if wrong, Asks the same question again
+            print("Wrong Answer, try again")
+            user_lives -= 1  
+            print ("Your score is now", score)
+    question.remove(ques)
